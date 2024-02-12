8c63474d838049a70756543f91256edceee20c89
Kuba <j.sirkowski@gmail.com>
2024-02-12 18:25:20 +0000

    Initial Commit

---
+++ GitHubExercise2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GitHubExercise2
+//
+//  Created by Jakub Sirkowski on 12/02/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitHubExercise2/GitHubExercise2App.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubExercise2App.swift
+//  GitHubExercise2
+//
+//  Created by Jakub Sirkowski on 12/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct GitHubExercise2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
